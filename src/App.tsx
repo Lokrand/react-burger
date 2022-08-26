190f21c22656cc30c4bd68bf9bc8cf1c37eb2e2b
Dima <Uchiha132132qqq@gmail.com>
2022-08-26 21:41:21 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import { AppHeader } from "./components/AppHeader/AppHeader";
+import "./App.css";
+import { BurgerIngredients } from "./components/BurgerIngredients/BurgerIngredients";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppHeader />
+      <BurgerIngredients />
+    </>
   );
 }
 
---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,40 @@
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+export const AppHeader = () => {
+  return (
+    <>
+    <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "#1C1C21",
+          margin: "40px 40px 0",
+        }}
+      >
+        <div
+          style={{ display: "flex", alignItems: "center", marginLeft: "20px" }}
+        >
+          <BurgerIcon type="secondary" />
+          <p style={{ marginLeft: "8px", marginRight: "49px" }}>Конструктор</p>
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <ListIcon type="secondary" />
+          <p style={{ marginLeft: "8px", marginRight: "132px" }}>
+            Лента заказов
+          </p>
+        </div>
+        <Logo />
+        <div
+          style={{ display: "flex", alignItems: "center", marginLeft: "312px" }}
+        >
+          <ProfileIcon type="secondary" />
+          <p style={{ marginLeft: "11px" }}>Личный кабинет</p>
+        </div>
+      </div>
+      </>
+  )
+}
