b17a6d147b927ebc663714b5d566092c1567b700
Dima <Uchiha132132qqq@gmail.com>
2022-10-23 19:55:48 +0300

    fix github pages - first page

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,14 @@ import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./services/reducers/index.js";
-import { BrowserRouter as Router, HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    {/* <HashRouter> */}
       <Router>
-        {/* <Route path="/" children={ */}
         <App />
-        {/* }></Route> */}
       </Router>
-    {/* </HashRouter> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById("root") as HTMLElement
